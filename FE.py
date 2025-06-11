9d3926bdfc3f9d36bd8f493a741d0544a8c0f931
acp8668 <acp8668@nyu.edu>
2025-06-11 11:45:44 -0400

    example 2 BC's Defined

--- FE.py
+++ FE.py
@@ -11,9 +11,6 @@ import sys
 if sys.platform == 'linux':
     import torch_sparse_solve 
 #-----------------------#
-# Testing testing
-# PLZ WORK
-# changes
 #%%  structural FE
 class StructuralFE:
     #-----------------------#
@@ -21,28 +18,28 @@ class StructuralFE:
         self.Emin = Emin;
         self.Emax = Emax;
         self.penal = penal;
-        self.nelx = nelx;
-        self.nely = nely;
-        self.ndof = 2*(nelx+1)*(nely+1)
+        self.nelx = nelx; # number of elements in x
+        self.nely = nely; # numer of elements in y
+        self.ndof = 2*(nelx+1)*(nely+1) # total DoF
         self.KE=self.getDMatrix();
-        self.fixed = fixed;
+        self.fixed = fixed; #fixed BC's
         self.free = np.setdiff1d(np.arange(self.ndof),fixed);
-        self.f = forceBC;
+        self.f = forceBC; #force vector
         self.edofMat=np.zeros((nelx*nely,8),dtype=int)
         for elx in range(nelx):
             for ely in range(nely):
-                el = ely+elx*nely
-                n1=(nely+1)*elx+ely
-                n2=(nely+1)*(elx+1)+ely
-                self.edofMat[el,:]=np.array([2*n1+2, 2*n1+3, 2*n2+2, 2*n2+3,2*n2, 2*n2+1, 2*n1, 2*n1+1])
+                el = ely+elx*nely #element number
+                n1=(nely+1)*elx+ely # bottom left node
+                n2=(nely+1)*(elx+1)+ely # bottom right node
+                self.edofMat[el,:]=np.array([2*n1+2, 2*n1+3, 2*n2+2, 2*n2+3,2*n2, 2*n2+1, 2*n1, 2*n1+1]) # assign DoF for element
 
-        self.iK = np.kron(self.edofMat,np.ones((8,1))).flatten()
-        self.jK = np.kron(self.edofMat,np.ones((1,8))).flatten()
+        self.iK = np.kron(self.edofMat,np.ones((8,1))).flatten() # rows
+        self.jK = np.kron(self.edofMat,np.ones((1,8))).flatten() # columns
 
     def getDMatrix(self):
         E=1
         nu=0.3
-        k=np.array([1/2-nu/6,1/8+nu/8,-1/4-nu/12,-1/8+3*nu/8,-1/4+nu/12,-1/8-nu/8,nu/6,1/8-3*nu/8])
+        k=np.array([1/2-nu/6,1/8+nu/8,-1/4-nu/12,-1/8+3*nu/8,-1/4+nu/12,-1/8-nu/8,nu/6,1/8-3*nu/8]) #stiffness coefficients for plane stress
         KE = E/(1-nu**2)*np.array([ [k[0], k[1], k[2], k[3], k[4], k[5], k[6], k[7]],
         [k[1], k[0], k[7], k[6], k[5], k[4], k[3], k[2]],
         [k[2], k[7], k[0], k[5], k[6], k[3], k[4], k[1]],
@@ -56,12 +53,12 @@ class StructuralFE:
     def solve(self, density):
 
         self.densityField = density
-        self.u=np.zeros((self.ndof,1))
+        self.u=np.zeros((self.ndof,1))  # displacement vector
         sK=((self.KE.flatten()[np.newaxis]).T*(self.Emin+(0.01 + density)**self.penal*(self.Emax-self.Emin))).flatten(order='F')
-        K = coo_matrix((sK,(self.iK,self.jK)),shape=(self.ndof,self.ndof)).tocsc()
+        K = coo_matrix((sK,(self.iK,self.jK)),shape=(self.ndof,self.ndof)).tocsc() #assembling global stiffness matrix
         K = K[self.free,:][:,self.free]
-
-        self.u[self.free,0]=spsolve(K,self.f[self.free,0])
+        self.u[self.free,0]=spsolve(K,self.f[self.free,0]) # solving equilibrium
+        # strain energy calculations
         self.Jelem = (np.dot(self.u[self.edofMat].reshape(self.nelx*self.nely,8),self.KE) * self.u[self.edofMat].reshape(self.nelx*self.nely,8) ).sum(1)
         return self.u, self.Jelem
 
@@ -69,12 +66,12 @@ class StructuralFE:
     def plotFE(self,figure_name):
          #plot FE results
          fig= plt.figure() # figsize=(10,10)
-         plt.subplot(1,2,1);
+         plt.subplot(1,2,1); #plot x-displacement
          im = plt.imshow(self.u[1::2].reshape((self.nelx+1,self.nely+1)).T, cmap=cm.jet,interpolation='none')
          J = ( (self.Emin+self.densityField**self.penal*(self.Emax-self.Emin))*self.Jelem).sum()
          plt.title('U_x , J = {:.2E}'.format(J))
          fig.colorbar(im)
-         plt.subplot(1,2,2);
+         plt.subplot(1,2,2); # plot y displacement
          im = plt.imshow(self.u[0::2].reshape((self.nelx+1,self.nely+1)).T, cmap=cm.jet,interpolation='none')
          fig.colorbar(im)
          plt.title('U_y')
--- main.py
+++ main.py
@@ -9,13 +9,25 @@ if(example == 1): #  MBBBeam
     exampleName = 'MBBBeam'
     ndof = 2*(nelx+1)*(nely+1) 
     dofs=np.arange(ndof) 
-    fixed= np.union1d(np.arange(0,2*(nely+1),2), 2*(nelx+1)*(nely+1)-2*(nely+1)+1)
-    forceBC = np.zeros((ndof,1)) 
-    forceBC[2*(nely+1)+1 ,0]=-1 
+    fixed= np.union1d(np.arange(0,2*(nely+1),2), 2*(nelx+1)*(nely+1)-2*(nely+1)+1) #fixing left edge horizontally and bottom right node vertically
+    forceBC = np.zeros((ndof,1)) #force vector with 0's
+    forceBC[2*(nely+1)+1 ,0]=-1 #Apply a downward force 1 unit on second node (from the left) along the bottom edge in y.
+
 elif(example == 2): #  TipCantilever, cantilever with a loading at bottom-right corner
     exampleName = 'TipCantilever'
     ndof = 2*(nelx+1)*(nely+1) 
-    dofs=np.arange(ndof) 
+    dofs=np.arange(ndof)
+
+    fixedNodes = np.arange(nely+1)  #gets nodes at left edge
+    fixedNodesX = 2*fixedNodes  # fixes left edge nodes in X direction
+    fixedNodesY = 2*fixedNodes + 1 #fixes left edge nodes in Y direction
+    fixed = np.union1d(fixedNodesX, fixedNodesY)  #creates array of fixed nodes
+
+    forceBC = np.zeros((ndof,1)) # creates a 1D column vector of 0's
+    forceNode = nelx*(nely+1)+0 # locates the node where force should be applied
+    force_dof_y = 2*forceNode+1 # makes it so force only acts in y direction
+    forceBC[force_dof_y,0] = -1 # assigns force value of -1 to the node in y direction
+
     ## specify the fixed boundary condition, fixed and force boundary condition, forceBC for MBBeam
 
 FE_solver = StructuralFE()
