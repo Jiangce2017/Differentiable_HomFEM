7c540b17d709979fbcf8b468fabf44a6c075d3bf
acp8668 <acp8668@nyu.edu>
2025-06-23 16:07:34 -0400

    initial testing

--- test_elasticity_tensor_2d.py
+++ test_elasticity_tensor_2d.py
@@ -1,7 +1,35 @@
 import numpy as np
+import h5py
 from elasticity_tensor_2d import elasticity
 
+def load_shapespace_example(index, shape_path='ShapeSpace.mat', prop_path='PropertySpace.mat' ):
+    with h5py.File(shape_path,'r') as f:
+        key = list(f.keys())[0]
+        dataset = f[key]
+        xPhys = dataset[:, :, index]
+        xPhys = xPhys.T
+
+    with h5py.File(prop_path,'r') as f:
+        key = list(f.keys())[0]
+        props = f[key]
+        C11 = props[0, index]
+        C12 = props[1, index]
+        C22 = props[2, index]
+        C66 = props[3, index]
+
+    Q_ex = np.array([[C11, C12, 0], [C12, C22, 0], [0, 0, C66]])
+
+    return xPhys, Q_ex
+
+
 if __name__ == '__main__':
-    xPhys = np.ones((50,50))
-    Q = elasticity(xPhys)
-    print(Q)
+    sample_index = 0
+    xPhys, Q_ex = load_shapespace_example(sample_index)
+
+    Q_code = elasticity(xPhys)
+    print(f"\n=== Sample #{sample_index} ===")
+    print("\nComputed Elasticity Tensor (Q_pred):")
+    print(np.round(Q_pred, 4))
+
+    print("\nReference Elasticity Tensor (Q_ref):")
+    print(np.round(Q_ref, 4))
