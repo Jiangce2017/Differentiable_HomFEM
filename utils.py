92924efc389068d11b04519d8d11b930a5fc6fe4
acp8668 <acp8668@nyu.edu>
2025-06-23 16:07:34 -0400

    initial testing

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import h5py
 
-def load_mat(filename):
-    with h5py.File(filename, 'r') as f:
+def load_mat(ShapeSpace):
+    with h5py.File(ShapeSpace, 'r') as f:
         data = {}
         for k, v in f.items():
             data[k] = v[:]  # Load data into memory
     return data
-    
+
+
