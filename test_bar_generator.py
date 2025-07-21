c77a910df9759ab5116a678435f8877b170dceed
Achez <achez@Jiangces-MBP.lan>
2025-07-21 12:37:52 -0400

    convert voxel to stl

--- test_bar_generator.py
+++ test_bar_generator.py
@@ -1,11 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from skimage import measure
-from stl import mesh
 import os
+import trimesh
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-def generate_dogbone_mask(length_mm=165, width_gauge_mm=13, width_ends_mm=19, length_gauge_mm=57, resolution=2): #0.5 is 2mm x 2mm and 1mm x1mm is 1
-    pixel_per_mm = resolution
+def generate_dogbone_mask(length_mm=165, width_gauge_mm=13, width_ends_mm=19, length_gauge_mm=57, pixel_per_mm = 20): 
+    
     gage_start_mm = (length_mm - length_gauge_mm) // 2
 
     width_gauge_px = width_gauge_mm * pixel_per_mm
@@ -26,7 +27,7 @@ def generate_dogbone_mask(length_mm=165, width_gauge_mm=13, width_ends_mm=19, le
 
     return domain
 
-def tile_lattice_to_mask(lattice_tile, mask):
+def tile_lattice_to_mask(lattice_tile, mask,tentile_direction):
     tiled = np.zeros_like(mask, dtype=np.uint8)
     tile_h, tile_w = lattice_tile.shape
     print("tile_h: {}, tile_w: {}".format(tile_h, tile_w))
@@ -34,40 +35,60 @@ def tile_lattice_to_mask(lattice_tile, mask):
     for y in range(0, mask.shape[0] - tile_h + 1, tile_h):
         for x in range(0, mask.shape[1] - tile_w + 1, tile_w):
             if mask[y:y+tile_h, x:x+tile_w].all():
-                tiled[y:y+tile_h, x:x+tile_w] = lattice_tile
+                if tentile_direction == 'y':
+                    tiled[y:y+tile_h, x:x+tile_w] = lattice_tile
+                elif tentile_direction == 'x':
+                    tiled[y:y+tile_h, x:x+tile_w] = lattice_tile.transpose()
 
     return tiled
 #Able to get unique file names for different dogbones
-def get_unique_filename(base_name="dogbone_lattice", ext=".stl", folder="results/dogbones"):
-    i = 0
+def get_unique_filename(tentile_direction,base_name="dogbone_lattice", ext=".stl", folder="results/dogbones"):
+    i = 1
     while True:
-        filename = f"{base_name}{i if i else ''}{ext}"
+        filename = f"{base_name}{'_'+tentile_direction+'_'}{i if i else ''}{ext}"
         full_path = os.path.join(folder, filename)
         if not os.path.exists(full_path):
             return full_path
         i += 1
 
-def extrude_and_export_stl(lattice_2d, thickness_mm, voxel_size_mm=1.0, filename="results/dogbones/dogbone_lattice.stl"):
+def extrude_and_export_stl(lattice_2d, filename="results/dogbones/dogbone_lattice.stl"):
     # Create 3D voxel array
-    voxels = np.repeat(lattice_2d[:, :, np.newaxis], int(thickness_mm), axis=2)
+    lattice_3d = np.repeat(lattice_2d[:, :, np.newaxis], 3, axis=2)
+    voxels = np.zeros((lattice_3d.shape[0]+2,lattice_3d.shape[1]+2,lattice_3d.shape[2]+2), dtype=np.uint8)
+    voxels[1:-1,1:-1,1:-1] = lattice_3d
 
     # Convert to mesh using marching cubes
     verts, faces, _, _ = measure.marching_cubes(voxels, level=0.5)
 
-    # Scale to mm
-    verts *= voxel_size_mm
+    # rescale to real dimensions
+    length_mm=165
+    width_ends_mm=19
+    thickness_mm = 3.2
+    target_dim = np.array([length_mm,width_ends_mm,thickness_mm])
+    verts = rescale_dimension(verts, target_dim)
 
-    # Create mesh and export
-    solid = mesh.Mesh(np.zeros(faces.shape[0], dtype=mesh.Mesh.dtype))
-    for i, f in enumerate(faces):
-        for j in range(3):
-            solid.vectors[i][j] = verts[f[j], :]
-
-    solid.save(filename)
+    mesh = trimesh.Trimesh(vertices=verts, faces=faces)
+    mesh.export(filename)
     print(f"STL file saved to {filename}")
 
+def rescale_dimension(verts, target_dim):
+    v_min = np.min(verts,axis=0)
+    v_max = np.max(verts,axis=0)
+    scale = target_dim/(v_max-v_min)
+    verts = (verts - v_min)*scale
+    return verts
+
+
+
+
 def main():
+    length_mm=166
+    width_gauge_mm=14
+    width_ends_mm=22
+    length_gauge_mm=58
     lattice_path = "results/lattice_output.npy"
+    tentile_direction = 'x'
+    
     if not os.path.exists(lattice_path):
         raise FileNotFoundError(f"Cannot find {lattice_path}")
 
@@ -78,23 +99,19 @@ def main():
     plt.axis('off')
     plt.show()
 
-    #resolution = lattice_tile.shape[0]
-    resolution = 10
-    print("resolution: {}".format(resolution))
-    domain_mask = generate_dogbone_mask(resolution=resolution)
-    tiled_lattice = tile_lattice_to_mask(lattice_tile, domain_mask)
-    print("tiled_lattice shape: {}".format(tiled_lattice.shape))
+    lattice_resolution = lattice_tile.shape[0]
+    lattice_size_mm = 2
+    pixel_per_mm = lattice_resolution // lattice_size_mm
+    domain_mask = generate_dogbone_mask(length_mm=length_mm, width_gauge_mm=width_gauge_mm, width_ends_mm=width_ends_mm, length_gauge_mm=length_gauge_mm,pixel_per_mm=pixel_per_mm)
+    tiled_lattice = tile_lattice_to_mask(lattice_tile, domain_mask,tentile_direction)
 
     # Optional: visualize the 2D layout
     plt.imshow(tiled_lattice, cmap='Greens')
     plt.title("Tiled Lattice in ASTM D638 Dogbone")
     plt.axis('off')
     plt.show()
-
-    # # User-defined thickness
-    # thickness_mm = float(input("Enter desired thickness (in mm): ")) #standard size is 3.2mm
-    # filename = get_unique_filename()
-    # extrude_and_export_stl(tiled_lattice, thickness_mm, filename=filename)
+    filename = get_unique_filename(tentile_direction)
+    extrude_and_export_stl(tiled_lattice, filename=filename)
 
 if __name__ == "__main__":
     main()
